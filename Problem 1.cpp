393c6fcaeb25e9019e718031f35b1e320be7284e
Abhishek Mukherjee <abhishekmukherjee1408@gmail.com>
2017-04-06 03:14:49 +0530

    Update Problem 1.cpp

--- Problem 1.cpp
+++ Problem 1.cpp
@@ -1,3 +1,14 @@
+/*
+The below algorithm is a modification of [Boyer–Moore majority vote algorithm]{https://en.wikipedia.org/wiki/Boyer%E2%80%93Moore_majority_vote_algorithm}
+Start with two empty candidate slots and two counters set to 0.
+for each item:
+    if it is equal to either candidate, increment the corresponding count
+    else if there is an empty slot (i.e. a slot with count 0), put it in that slot and set the count to 1
+    else reduce both counters by 1
+At the end, make a second pass over the array to check whether the candidates really do have the required count.
+*/
+
+
 int Solution::repeatedNumber(const vector<int> &A) {
     int c1 = 0, c2 = 0;
     int a, b, n = A.size();
